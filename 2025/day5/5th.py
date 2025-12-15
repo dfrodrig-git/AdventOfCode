9d7f49c6e5737dfbe2a6f223aefec43eb9d4205e
dfrodrig <danielcr@ieee.org>
2025-12-15 15:48:11 +0000

    Day 5 both solved!

--- 2025/day5/5th.py
+++ 2025/day5/5th.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 
-testRun = True
+testRun = False
 
 def assertExpected( result, expected,part=1):
     logger.info(f'Result part:{part}: {result}, expected: {expected}')
@@ -15,7 +15,7 @@ def assertExpected( result, expected,part=1):
         logger.critical("Unnaceptable!")
 
 
-def getInput(test = True, day = None) :
+def getInput(test = True, day = 5) :
     if testRun or test :
         f=open(f'inputDay{day}Test.txt')
     else :
@@ -25,36 +25,98 @@ def getInput(test = True, day = None) :
     #strip/split
     ''' 
     lines = [line.strip() for line in f]
-    data = [x.split("-") for x in lines]
+    
+    ranges = [list(map(int, x.split("-"))) for x in lines if "-" in x]
+    
+    ingredients = [int(x) for x in lines if "-" not in x and len(x)>0]
+    
+        
 
-    return data
+    return ranges, ingredients
 
-    ''' 
-    # regex
-    ''' 
-    data = f.read()
-    p = re.compile()
+def checkIngredientInRange(ingredient, rangeMap) :
+    if ingredient in range(*rangeMap) :
+        return True
 
-    return p.findall(data)
+def processDataPart1(ranges, ingredients) :
+    
+    freshIngredients = []
+    
+    for ingredient in ingredients :
+        for rangeMap in ranges :
+            freshIngredients.append(ingredient) if ingredient in range(rangeMap[0], rangeMap[1]+1) else None
+    
+    #print(f'freshIngredients: {freshIngredients}')
 
-def processDataPart1(data) :
-    pass
+    return len(set(freshIngredients))
 
+class Range :
+    def __init__(self,start, end) :
+        self.start = start
+        self.end = end
+        self.nextNode = None
+        self.orphan = False
+    
+    def setNextNode(self, nextNode) :
+        self.nextNode = nextNode
+    
+    def updateRange(self) :
+        if self.nextNode is None :
+            return None
+        
+        #handle overlap
+        if self.end >= self.nextNode.start :
+            if self.nextNode.end > self.end :
+               self.end = self.nextNode.end
+            self.nextNode.orphan = True
+            self.nextNode = self.nextNode.nextNode
+       
+            return self
+        
+        return self.nextNode
+                                  
+def processDataPart2(ranges) :
+    
+    rangeNodes = []
+    for rangeMap in ranges :
+        newRange = Range(rangeMap[0], rangeMap[1])
+        rangeNodes.append(newRange)
 
-def processDataPart2(data) :
-    pass
-
-
+    rangeNodes.sort(key= lambda x: x.start)
+    
+    #created Linked List
+    [x.setNextNode(y) for x,y in zip(rangeNodes[:-1], rangeNodes[1:])  ]
+    
+    #remove overlaps
+    nextNode = rangeNodes[0]
+    
+    while nextNode.nextNode is not None :
+        nextNode = nextNode.updateRange()
+        
+    #check new List
+    newRanges = [x for x in rangeNodes if x.orphan==False]
+    
+    #compute result:
+    #result= sum( [ x.end-x.start+1  for x in newRanges ])
+    
+    print(f'{[(x.start, x.end) for x in newRanges]}')
+    
+    return newRanges
+    
 #Part1
 #do stuff
-data = getInput(testRun) 
+ranges, ingredients = getInput(testRun) 
 
-result = processDataPart1(data) 
+print(f'ranges: {ranges}')
+print(f'ingredients: {ingredients}')
 
-assertExpected(result, None)
+result = processDataPart1(ranges, ingredients) 
+
+assertExpected(result, 3)
 
 #Part 2
-result2 = processDataPart2(data) 
+result2 = processDataPart2(ranges) 
 
-assertExpected(result2, None, part=2)
+assertExpected(result2, 14, part=2)
 
+ 
